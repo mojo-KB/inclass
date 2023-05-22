e3a0153541e3a67bf339e0d1ca1cd4d02c33af75
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2023-05-21 21:45:52 -0400

    update files

--- inclass/src/components/QuestionCard2.tsx
+++ inclass/src/components/QuestionCard2.tsx
@@ -5,11 +5,12 @@ type Props = {
     signalTime: string
 }
 function QuestionCard2({ content, signalTime }: Props) {
+    const [showReply, setShowReply] = React.useState(false)
     const handleClick = () => {
 
     }
     return (
-        <div className='m-0'>
+        <div className='m-1'>
             <div className='min-w-full bg-gray-900 text-white p-10  rounded-lg flex items-center justify-between' onClick={handleClick} >
 
                 <div className='flex items-center space-x-5'>
@@ -20,15 +21,17 @@ function QuestionCard2({ content, signalTime }: Props) {
                 <div className='font-bold text-red-500'>{signalTime}</div>
             </div>
 
-            <div className=''>
+            <div className='hidden inline-block'>
                 <div className='text-white m-1 mx-20 p-3 rounded-lg shadow-sm flex items-center justify-between'>
                     <input type="text" className='flex-1 bg-transparent border-gray-700 border-2 text-white rounded-full p-1 px-3' />
                     <button className='secondaryBtn2 font-normal text-sm ml-3'>Reply</button>
                 </div>
+                <div className='mb-5'>
+                    <div className='text-white bg-gray-900 m-1 mx-20 p-3 rounded-lg shadow-sm'>Respones</div>
+                </div>
+
             </div>
-            <div className='mb-5'>
-                <div className='text-white bg-gray-900 m-1 mx-20 p-3 rounded-lg shadow-sm'>Respones</div>
-            </div>
+
             <div>
             </div>
 
